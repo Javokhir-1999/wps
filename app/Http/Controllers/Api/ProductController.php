1c131cf742ed598c56a2f7dd57a4c5365ce2dfe6
Javox1999 <45287031+Javox1999@users.noreply.github.com>
2022-05-23 23:42:58 +0500

    wps

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -8,22 +8,130 @@ use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
-    public function getExpenseByProduct(array $product){
-       return DB::select(
-            "SELECT warehouse.id as warehouse_id, materials.name, warehouse.remainder - product_materials.quantity*? as qty 
-            FROM warehouse, materials, product_materials 
-            WHERE materials.id = warehouse.material_id 
-                AND materials.id = product_materials.material_id
-                AND product_id IN(SELECT products.id FROM products WHERE products.name =?)
-            ", 
+    // what materials was used from which warehause and how much
+    protected $usedMaterialsHistory = [];
 
-            [$product['product_qty'],$product['product_name']]);
+    // The actual list of warehoses to be used (for each material)
+    protected $actualWarehouseForMaterials = [];
+
+    // save reports about changes
+    protected $materialsUsed = [];
+
+    // 
+    protected $usedWarehouseIDs = array( 
+        '1' => 0,
+        '2' => 0,
+        '3' => 0,
+        '4' => 0,
+        '5' => 0,
+    );
+
+
+    //
+    public function getMaterials(object $materialNeeded){
+
+        // get actual warehouse for this: $material (from where to get)
+
+        $warehouseToUse = $this->getActualWarehouse($materialNeeded);
+        if($warehouseToUse){
+            // getting...
+            $warehouse = DB::table('warehouse')
+                            ->join('materials', 'warehouse.material_id', '=', 'materials.id')
+                            ->where('warehouse.id', $warehouseToUse['warehouse_id'])
+                            ->get(['warehouse.id AS warehouse_id','materials.name AS material_name','warehouse.remainder AS qty','price'], )
+                            ->first();                   
+
+            if($warehouse->qty >= $materialNeeded->qty){
+                $warehouse->qty = $warehouse->qty - $materialNeeded->qty;
+
+                // save used materials to report
+                array_push($this->materialsUsed, $warehouse);
+
+                $warehouse->qty = $materialNeeded->qty;
+                
+            }else{
+                // calculate new $qty by subtructing used materials // make qty positive by * (-1)
+                $materialNeeded->qty = -1 * ($warehouse->qty - $materialNeeded->qty); 
+
+                 // save used materials to report
+                array_push($this->materialsUsed, $warehouse);
+               
+                $this->usedWarehouseIDs[$materialNeeded->id] = $warehouse->warehouse_id;
+
+                $this->getMaterials($materialNeeded);
+            }
+        }else{
+
+            return false;
+        }
+
+
+    }
+
+    // get actual warehouse ID for this: $material
+    public function getActualWarehouse(object $material){ 
+
+        // history of used materils yet
+        $wID = $this->usedWarehouseIDs[$material->id];
+        
+
+        // get first shipped group of matterials
+        $warehouse = DB::table('warehouse')
+            ->where([['material_id', $material->id], ['id', '>', $wID]])
+            ->first();
+        if($warehouse)
+            return [
+                'warehouse_id' => $warehouse->id
+            ];
+        else
+            return False;
+    }
+
+    // change actual warehouse ID to get this: $material
+    public function changeActualWarehouse(object $material){
+        // array_push($actualWarehouseForMaterials => $material->id); 
+    }
+
+    public function getFromWarehouse(array $materials){ 
+
+        foreach($materials as $material){
+            do{           
+                //need Materils? -> getting...    
+                $needMoreMaterils = $this->getMaterials($material);
+
+                // keep getting while we get enough materials 
+            }while ($needMoreMaterils);
+
+ 
+        }
+        // report used materils
+        return $this->materialsUsed;
+    }
+
+    public function getExpenseByProduct(array $product){
+
+        // get product ID by product name 
+        $productID = DB::table('products')->where('name', $product['product_name'])->get()->first();
+
+        $productQty = $product['product_qty'];
+
+        // calculate and get total materials needed for this: $productQty amount of products
+        $materials = DB::select(
+            DB::RAW("
+                    SELECT materials.id, materials.name, quantity * $productQty AS qty 
+                    FROM materials, product_materials 
+                    WHERE product_id = $productID->id 
+                            AND materials.id = product_materials.material_id"));
+
+        // now get materials for this: $product from warehouse
+        return $this->getFromWarehouse($materials);
     }
 
     public function makeProduct(Request $request){
 
         $products = $request->products;
         $result = [];
+
         foreach ($products as $product)
             array_push($result, [ 
                 "product_name" => $product["product_name"],
